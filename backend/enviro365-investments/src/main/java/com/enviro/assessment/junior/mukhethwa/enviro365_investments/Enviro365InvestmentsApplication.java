a6ee2e0a2d2affe213b872ff21e703950152a84e
Mukhethwa <masikngl@gmail.com>
2026-09-08 19:55:53 +0200

    Initialize Enviro365 Investments project with Maven setup and basic application structure

---
+++ backend/enviro365-investments/src/main/java/com/enviro/assessment/junior/mukhethwa/enviro365_investments/Enviro365InvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.mukhethwa.enviro365_investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365InvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365InvestmentsApplication.class, args);
+	}
+
+}
---
+++ backend/enviro365-investments/src/test/java/com/enviro/assessment/junior/mukhethwa/enviro365_investments/Enviro365InvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.mukhethwa.enviro365_investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365InvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
